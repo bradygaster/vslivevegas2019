7eb4e1b50878c6fc156cf05232c10ae85567fcfd
Brady Gaster <bradyg@microsoft.com>
2019-03-07 12:06:27 -0800

    initial creation

---
+++ ClimateControlSystem/FunctionApp/MessageReceivedFunction.cs
@@ -0,0 +1,41 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.EventHubs;
+using System.Text;
+using System.Net.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using System.IO;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionApp
+{
+    public static class MessageReceivedFunction
+    {
+        private static HttpClient client = new HttpClient();
+
+        [FunctionName("MessageReceivedFunction")]
+        [return: Queue("incoming", Connection = "AzureWebJobsStorage")]
+        public static async Task<string> RunAsync(
+            [EventHubTrigger("climatedevicehub", 
+                Connection = "IoTConnectionString")] EventData message, 
+            [SignalR(HubName="climate")] IAsyncCollector<SignalRMessage> messages,
+            ILogger log)
+        {
+            log.LogInformation($"C# IoT Hub trigger function processed a message: " +
+            	    "{Encoding.UTF8.GetString(message.Body.Array)}");
+
+            string json = Encoding.UTF8.GetString(message.Body.Array);
+
+            await messages.AddAsync(new SignalRMessage {
+                Target = "climateDataReceived",
+                Arguments = new [] { json }
+            });
+
+            log.LogInformation($"Sent temperature data to SignalR.");
+
+            return json;
+        }
+    }
+}
